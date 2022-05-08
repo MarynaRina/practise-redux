59e3b5aa405066f4cad6cd0b8e4b5911a13a8eb2
MarynaRina <risaranma@gmail.com>
2022-05-08 23:56:53 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='container'>
+                <input className='input' type='text' placeholder='your text' />
+                <button className='btn'>Add</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
